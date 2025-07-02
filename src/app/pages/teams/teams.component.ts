51592808a8f033341695c7b521f1fc88c6a21e1a
vatsaljain-hub <vatsaljain46@gmail.com>
2025-07-02 20:15:44 +0530

    change image in bg color

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -10,10 +10,10 @@ import { CommonModule } from '@angular/common';
 })
 export class TeamsComponent {
   organizers = [
-    { name: 'Antrixsh Gupta', image: 'assets/teams/antrixsh_gupta.jpg' },
-    { name: 'Shailesh Chaskar', image: 'assets/teams/shailesh_chaskar.jpg' },
-    { name: 'Pratik Kale', image: 'assets/teams/pratik.JPG' },
-    { name: 'Dnyanada Mahajan', image: 'assets/teams/dnyanada_mahajan.jpg' },
+    { name: 'Antrixsh Gupta', image: 'assets/teams/antrixsh_gupta.JPG' },
+    { name: 'Shailesh Chaskar', image: 'assets/teams/shailesh2.jpg' },
+    { name: 'Pratik Kale', image: 'assets/teams/pratik2.jpg' },
+    { name: 'Dnyanada Mahajan', image: 'assets/teams/dnyanda2.jpg' },
   ];
 
   volunteers = [
