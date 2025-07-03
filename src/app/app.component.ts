556e2a6bf2aef3b99c02b926e35208e30c9f989c
Gitesh08 <gmahadik8080@gmail.com>
2025-07-04 02:24:49 +0530

    added route 'photo-frame'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./components/header/header.component";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { SocialComponent } from "./components/social/social.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent, SocialComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'ccd25';
+  title = "ccd25";
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
-import { FooterComponent } from '../../components/footer/footer.component';
-import { HeaderComponent } from '../../components/header/header.component';
-import { HeroComponent } from '../../components/hero/hero.component';
-import { SocialComponent } from '../../components/social/social.component';
-import { VenueComponent } from '../../components/venue/venue.component';
-import { WhatIsCcdComponent } from '../../components/what-is-ccd/what-is-ccd.component';
-import { CoolFiguresComponent } from '../../components/cool-figures/cool-figures.component';
-import { PastSpeakersComponent } from '../../components/past-speakers/past-speakers.component';
-import { VideoFrameComponent } from '../../components/video-frame/video-frame.component';
-import { PastPhotosComponent } from '../../components/past-photos/past-photos.component';
-import { SponsorsComponent } from '../../components/sponsors/sponsors.component';
+import { Component } from "@angular/core";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { HeaderComponent } from "../../components/header/header.component";
+import { HeroComponent } from "../../components/hero/hero.component";
+import { SocialComponent } from "../../components/social/social.component";
+import { VenueComponent } from "../../components/venue/venue.component";
+import { WhatIsCcdComponent } from "../../components/what-is-ccd/what-is-ccd.component";
+import { CoolFiguresComponent } from "../../components/cool-figures/cool-figures.component";
+import { PastSpeakersComponent } from "../../components/past-speakers/past-speakers.component";
+import { VideoFrameComponent } from "../../components/video-frame/video-frame.component";
+import { PastPhotosComponent } from "../../components/past-photos/past-photos.component";
+import { SponsorsComponent } from "../../components/sponsors/sponsors.component";
+
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [
     HeaderComponent,
@@ -26,7 +27,7 @@ import { SponsorsComponent } from '../../components/sponsors/sponsors.component'
     PastPhotosComponent,
     SponsorsComponent,
   ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.scss",
 })
 export class HomeComponent {}
