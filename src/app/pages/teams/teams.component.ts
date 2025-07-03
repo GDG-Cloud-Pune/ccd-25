66b495b23062ef73446380d792b65a604cc6f5a0
vatsaljain-hub <vatsaljain46@gmail.com>
2025-07-03 12:08:43 +0530

    yash khope image change

--- src/app/pages/teams/teams.component.ts
+++ src/app/pages/teams/teams.component.ts
@@ -54,7 +54,7 @@ export class TeamsComponent {
 { name: 'Vihaa Terkar', image: 'assets/teams/Vihaa_Terkar.png' },
 { name: 'Yash Bade', image: 'assets/teams/Yash_Bade.png' },
 { name: 'Yash Gwalvanshi', image: 'assets/teams/Yash_Gwalvanshi.png' },
-{ name: 'Yash Khope', image: 'assets/teams/Yash_Khope.png' },
+{ name: 'Yash Khope', image: 'assets/teams/Yash_Khope.PNG' },
 { name: 'Yash Londhe', image: 'assets/teams/Yash_Londhe.png' }
 
 ];
