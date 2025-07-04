1a9e2cd2832264b9da30d4e469ef2a1d369e29a8
Pratik Kale <ppvkale@gmail.com>
2025-07-05 00:08:49 +0530

    Badge route on Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,47 +1,47 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: "",
-    redirectTo: "home",
-    pathMatch: "full",
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
-    path: "home",
+    path: 'home',
     loadComponent: () =>
-      import("./pages/home/home.component").then((m) => m.HomeComponent),
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: "tickets",
+    path: 'tickets',
     loadComponent: () =>
-      import("./pages/tickets/tickets.component").then(
-        (m) => m.TicketsComponent,
+      import('./pages/tickets/tickets.component').then(
+        (m) => m.TicketsComponent
       ),
   },
   {
-    path: "code-of-conduct",
+    path: 'code-of-conduct',
     loadComponent: () =>
-      import("./pages/code-of-conduct/code-of-conduct.component").then(
-        (m) => m.CodeOfConductComponent,
+      import('./pages/code-of-conduct/code-of-conduct.component').then(
+        (m) => m.CodeOfConductComponent
       ),
   },
   {
-    path: "speakers",
+    path: 'speakers',
     loadComponent: () =>
-      import("./pages/speakers/speakers.component").then(
-        (m) => m.SpeakersComponent,
+      import('./pages/speakers/speakers.component').then(
+        (m) => m.SpeakersComponent
       ),
   },
   {
-    path: "photo-frame",
+    path: 'badge',
     loadComponent: () =>
-      import("./pages/photo-frame/photo-frame.component").then(
-        (m) => m.PhotoFrameComponent,
+      import('./pages/photo-frame/photo-frame.component').then(
+        (m) => m.PhotoFrameComponent
       ),
   },
   {
-    path: "teams",
+    path: 'teams',
     loadComponent: () =>
-      import("./pages/teams/teams.component").then((m) => m.TeamsComponent),
+      import('./pages/teams/teams.component').then((m) => m.TeamsComponent),
   },
 ];
